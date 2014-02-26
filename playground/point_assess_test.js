f1c0b50b0899d75372edcb7d974a1faf19313158
Ben Carlson <ben.s.carlson@gmail.com>
2014-02-26 16:15:57 -0500

    update to use max reducer

--- playground/point_assess_test.js
+++ playground/point_assess_test.js
@@ -2,8 +2,6 @@
 //github.com/MapofLife/forest-tool/blob/master/playground/point_assess_test.js
 centerMap(-114,33,6);
 
-//new approach
-
 var fcSonora = ee.FeatureCollection('ft:1Ec8IWsP8asxN-ywSqgXWMuBaxI6pPaeh6hC64lA')
   .filter(ee.Filter.eq('ECO_NAME', 'Sonoran desert'));
 
@@ -13,36 +11,48 @@ var fcPointPoly = new ee.FeatureCollection([
             [[-117, 35], [-112, 35], [-112, 28], [-117, 28]]))
     ]);
 
-//take a collection of random points.  initialize the viz property to 0.
-var points = ee.FeatureCollection.randomPoints({region:fcPointPoly, points:100});
+//take a collection of random points.
+var points = ee.FeatureCollection.randomPoints({region:fcPointPoly, points:200});
 
-print(points.getInfo());
+//buffer points by 10km
 var pointsBuf = points.map(function(f){return f.buffer(10000)});
 
+/////
+// create the range and refined range
+/////
+
 var range = ee.Image('srtm90_v4').clip(fcSonora);
-var refined = range.gte(400);
+
+//take only values greater than 400m.  Number is arbitrary just to create a range map
+var refined = range.gte(400); 
 refined = refined.mask(refined);
 
-//sum the values under the buffered points.  any sums > 0 intersect the range map
-var inExpRange0 = range.reduceRegions(pointsBuf,ee.Reducer.histogram(),1000);
-                          //.filter(ee.Filter.gt('sum',0));
-//inExpRange0 = inExpRange0.map(function(f) {
-//                return f.set({'inExp': 1});
-//              });
+//we need to set up the data so that range has 1, refined has 2
+range = range.subtract(range); //set up range so that its 0
+range = range.add(1); //now add one 
+//refined is masked so we can't just add.  instead set to 2 where its currently 1
+refined = refined.where(refined.eq(1),2); 
+
+//rename bands to something more descriptive
+range = range.select(['elevation'],['habitat']);
+refined = refined.select(['elevation'],['habitat']);
+
+var allrange = ee.ImageCollection.fromImages([range,refined])
+                  .reduce(ee.Reducer.max());
+
+////
+// run the analysis
+////
+
+//now we have the layer that has 1 for original range and 2 for refined range
+//use reduce regions with the buffered points to 
+var inExpRange0 = allrange.reduceRegions(pointsBuf,ee.Reducer.max(),1000);
+
+//I would expect each feature to have a "max" property, but only some do.
 print(inExpRange0.getInfo());              
-var inExpRange = inExpRange0.aggregate_count('sum');
 
-
-var inRefRange = refined.reduceRegions(pointsBuf,ee.Reducer.sum(),1000)
-              .filter(ee.Filter.gt('sum',0)).aggregate_count('sum');
-
-var inExp = inExpRange.getInfo();
-var inRef = inRefRange.getInfo();
-print("In expert: " + inExp);
-print("In refined: " + inRef);
-print("In expert range but not in refined range: " + (inExp - inRef));
-
-addToMap(range,{palette:"4C7A01"});
-addToMap(refined,{palette:"05A351"});
-addToMap(pointsBuf);
-addToMap(points);
+//addToMap(range,{palette:"4C7A01"}, "Range");
+//addToMap(refined,{palette:"05A351"}, "Refined");
+addToMap(allrange,{palette:"000000,9BF2F2,52238C",min:0,max:2},"All Range");
+addToMap(pointsBuf,{},"Buffers");
+addToMap(points,{},"Points");
