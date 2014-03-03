d51588f124663a7c4c7a525efeff93991ae1d9ec
Ben Carlson <ben.s.carlson@gmail.com>
2014-03-03 16:52:27 -0500

    handle an array of maps engine assets

--- playground/range_refine_test.js
+++ playground/range_refine_test.js
@@ -1,69 +1,43 @@
 //range_refine_test
 //https://github.com/MapofLife/forest-tool/blob/master/playground/range_refine_test.js
-//sample using Abeomelomys_sevia
-centerMap(144.86572,-5.92204, 8);
-
-//MAKE RANGE
-//04040405428907908306-04550633376616161003 Abeomelomys_sevia range
-var range =  ee.Image('GME/images/04040405428907908306-04550633376616161003');
-addToMap(range, {opacity: 0.3, palette: 'ff3366'}, 'range');
+centerMap(-112,47, 8);
 
 //SET VAR INPUT PARAMETERS
-var forestCover = 75; //greater than
-var MODIS0      = 1; 
-var MODIS1      = 1; 
-var Elevation   = 2000; //greater than
+var forestCoverLow = 25; //greater than this
+var forestCoverHigh = 50; //less than this
+var elevationLow = 1800; //greater than
+var elevationHigh = 20000; //less than
 
-//CREATE 4 VARS
-//1) % forest cover
-var forest = ee.Image('GME/images/04040405428907908306-09310201000644038383');
-forest = forest.divide(100);
-//addToMap(forest,{palette:'FCB360,065202',min:0,max:100},"% Forest Cover");
-var forFilter = range.where(forest.lt(forestCover),0);
-var out = forFilter.where(forest.gte(forestCover),1);
-//addToMap(forFilter,{ palette: 'cccccc,ff6666', max:1, min:0}, 'forest');
+var prefs = [0,1,3]; //use to select which habitat prefs.  this will be passed in by the user
 
-//2) MODIS Land Cover (2 vars)
-var modis0 = ee.Image('GME/images/04040405428907908306-03680284325645907752');
-//addToMap(modis0,{palette:'FFFFFF,021E52',min:0,max:1},"MODIS 0");
-var mod0Filter = range.where(modis0.eq(MODIS0),1);
-//addToMap(mod0Filter,{ palette: 'cccccc,ff6666', max:1, min:0}, 'modis0');
+//maps engine id's for the assests.  there should be a total of 12, only using 4 right now.
+var modis_binary = {
+  0:'04040405428907908306-03680284325645907752',
+  1:'04040405428907908306-12923493437973401200',
+  2:'04040405428907908306-16898429718931540991',
+  3:'04040405428907908306-16395992718068236206'
+};
 
-var modis1 = ee.Image('GME/images/04040405428907908306-12923493437973401200');
-//addToMap(modis1,{palette:'FFFFFF,036E52',min:0,max:1},"MODIS 1");
-var mod1Filter = range.where(modis1.eq(MODIS1),1);
-//addToMap(mod1Filter,{ palette: 'cccccc,ff6666', max:1, min:0}, 'modis1');
+//Abeomelomys_sevia range 04040405428907908306-04550633376616161003 centerMap(144.86572,-5.92204, 4);
+//Chrysothlypis_chrysomelas (Black-And-Yellow Tanager) 04040405428907908306-07422276925927616232 centerMap(-80,7, 6);
+//Heterodon nasicus (Nasicus) 04040405428907908306-09733671023748783803
+var range =  ee.Image('GME/images/04040405428907908306-09733671023748783803');
+var forest = ee.Image('GME/images/04040405428907908306-09310201000644038383')
+                .divide(100); //need to divide by 100 then we get two digit %
+var elev = ee.Image('GME/images/04040405428907908306-08319720230328335274');
+var habitat = ee.Image(0); //initialize habitat image
 
-//3) Elevation
-//Look in python code for the id
-var elevationlayer = ee.Image('GME/images/04040405428907908306-08319720230328335274');
+//make the landcover layer by merging modis layers for each of the habitat preferences
+for(var i=0; i<prefs.length; i++) {
+  habitat = habitat.add(ee.Image('GME/images/' + modis_binary[prefs[i]]).mask(range));
+}
 
-var ElvRange = elevationlayer.mask(range);
-//addToMap(ElvRange, "elvRange");
-var ElvFilter = range.where(elevationlayer.lt(Elevation),0)
-var out = ElvFilter.where(elevationlayer.gte(Elevation),1)
-//addToMap(ElvFilter,{ palette: 'cccccc,ff6666', max:1, min:0}, 'Elevation');
+//this will need to change, if we consider % forest cover in a more complex way.  good enough for now though
+habitat = habitat.gte(1)//since we are just adding all the habitat layers, we might end up with pixels > 1
+            .where(forest.lt(forestCoverLow).or(forest.gt(forestCoverHigh)),0) //remove all pixels outside the bounds of forest cover
+            .where(elev.lt(elevationLow).or(elev.gt(elevationHigh)),0); //remove all pixels outside the bound of elevation
 
-
-//COMPILE INTO ONE SURFACE with 4 bands
-var two   = forFilter.add(mod0Filter);
-var three = two.add(mod1Filter);
-var four  = three.add(ElvFilter);
-addToMap(four, {palette: '036E52, cccccc, 33ffff, 33ff66', max:4, min: 0}, 'refined surface');
-//I am confused about the line below,its outputtig everything above 1 and making the rest 0 I think
-var four = four.where(four.gte(4),1)
-var four = four.where(four.lte(1),0)
-addToMap(four, {palette: '330099,33ffff', max:1, min:0}, 'refined'); 
-
-//MAKE THE ranges surface
-//we need to set up the data so that range has 1, refined has 2
-range = range.subtract(range); //set up range so that its 0
-range = range.add(1); //now add one 
-//refined is masked so we can't just add.  instead set to 2 where its currently 1
-var refined = four.where(four.gte(1),2); 
-
-var allrange = ee.ImageCollection.fromImages([range,refined])
-                  .reduce(ee.Reducer.max())
-                  .mask(ee.Image(1));
-
-addToMap(allrange);
+addToMap(elev,{palette:'FFFFFF,403BCC',min:0,max:10000},"Elevation");
+addToMap(forest,{palette:'FCB360,065202',min:0,max:100},"% Forest Cover");
+addToMap(range, {opacity: 0.3, palette: 'FFFFFF,ff3366',min:0,max:1}, 'range');
+addToMap(habitat, {palette:'FFFFFF,036E52',min:0,max:1},"habitat");
